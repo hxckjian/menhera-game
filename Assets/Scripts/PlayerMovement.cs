57d72171e638e35724a5539c24d890dad135eab0
Teh Hock Jian <hockjianteh@gmail.com>
2025-05-12 13:42:34 +0800

    Added movement with tile collision

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,68 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int speed = 10;
-    private Rigidbody2D characterBody;
-    private Vector2 velocity;
-    private Vector2 inputMovement;
+    public float moveSpeed = 8f;
 
-    void Start() 
-    {
-        velocity = new Vector2(speed, speed);
-        characterBody = GetComponent<Rigidbody2D>();
-    }
+    public float collisionOffset = 0.05f;
 
-    void Update()
+    public ContactFilter2D movementFilter;
+
+    Vector2 movementInput;
+
+    Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    void Start()
     {
-        inputMovement = new Vector2 (
-            Input.GetAxisRaw("Horizontal"),
-            Input.GetAxisRaw("Vertical")
-        );
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        Vector2 delta = inputMovement * velocity * Time.deltaTime;
-        Vector2 newPosition = characterBody.position + delta;
-        characterBody.MovePosition(newPosition);
+        if (movementInput != Vector2.zero) {
+
+            int count = rb.Cast(
+                movementInput,
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset
+            );
+
+            if(count == 0) {
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
     }
+
+    void OnMove(InputValue movementValue) {
+        movementInput  = movementValue.Get<Vector2>();
+    }
+    // public int speed = 10;
+    // private Rigidbody2D characterBody;
+    // private Vector2 velocity;
+    // private Vector2 inputMovement;
+
+    // void Start() 
+    // {
+    //     velocity = new Vector2(speed, speed);
+    //     characterBody = GetComponent<Rigidbody2D>();
+    // }
+
+    // void Update()
+    // {
+    //     inputMovement = new Vector2 (
+    //         Input.GetAxisRaw("Horizontal"),
+    //         Input.GetAxisRaw("Vertical")
+    //     );
+    // }
+
+    // private void FixedUpdate()
+    // {
+    //     Vector2 delta = inputMovement * velocity * Time.deltaTime;
+    //     Vector2 newPosition = characterBody.position + delta;
+    //     characterBody.MovePosition(newPosition);
+    // }
 }
