bcce49d5f421fa0eefcd35b3f5010d0423c518f0
hxckjian <hockjianteh@gmail.com>
2025-05-26 17:52:00 +0800

    - Tested canvas popup jumpscare - Able to trigger sample jumpscare with button

---
+++ Assets/Scripts/JumpScareScript.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class JumpScareScript : MonoBehaviour
+{
+    [Header("Canvas Settings")]
+    public CanvasGroup jumpscareGroup;
+
+    [Header("Animator Settings")]
+    public Animator jumpscareAnimator;
+
+    private bool hasPlayed = false;
+
+     void Start()
+    {
+        HideJumpscare(); // Make sure it starts hidden
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)  && !hasPlayed)
+        {
+            ShowJumpscare();
+        }
+    }
+
+    void ShowJumpscare()
+    {
+        jumpscareGroup.alpha = 1f;
+        jumpscareGroup.interactable = true;
+        jumpscareGroup.blocksRaycasts = true;
+
+        // Play animation
+        // jumpscareAnimator.Play("Jumpscare");
+        jumpscareAnimator.SetTrigger("PlayJumpscare");
+
+        hasPlayed = true;
+    }
+
+    void HideJumpscare()
+    {
+        jumpscareGroup.alpha = 0f;
+        jumpscareGroup.interactable = false;
+        jumpscareGroup.blocksRaycasts = false;
+    }
+}
