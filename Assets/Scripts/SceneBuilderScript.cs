32edf2cd5a2ecb79a30d2f367d66f05633e16ac2
Teh Hock Jian <hockjianteh@gmail.com>
2025-05-26 15:28:31 +0800

    Configured Appropriate Screen Size for game

---
+++ Assets/Scripts/SceneBuilderScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SceneBuilderScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Screen.SetResolution(1920, 1080, false);
+        Screen.SetResolution(960,720,false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
