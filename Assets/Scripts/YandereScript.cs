5cc8dd17afdd0d9200e70ba5e427e8cebba21806
poonseyboon <shenliechong@gmail.com>
2025-05-28 21:40:03 +0800

    implementation of start menu

--- Assets/Scripts/YandereScript.cs
+++ Assets/Scripts/YandereScript.cs
@@ -5,17 +5,35 @@ public class YandereAI : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    NavMeshAgent agent;
+    private NavMeshAgent agent;
+    private Animator animator;
+    private Vector2 lastPosition;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+
+        animator = GetComponent<Animator>();
+        lastPosition = transform.position;
     }
 
     private void Update()
     {
-        agent.SetDestination(target.position);
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+
+        // Calculate movement direction for animation
+        Vector2 currentPosition = transform.position;
+        Vector2 direction = (currentPosition - lastPosition).normalized;
+
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+
+        lastPosition = currentPosition;
     }
 }
