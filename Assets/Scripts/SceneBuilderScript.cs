7ad8569329ed36040c4092dd0c39a6197a4fdce4
hxckjian <hockjianteh@gmail.com>
2025-06-26 15:39:03 +0800

    Added audio and changed startmenu camera

--- Assets/Scripts/SceneBuilderScript.cs
+++ Assets/Scripts/SceneBuilderScript.cs
@@ -5,6 +5,6 @@ public class SceneBuilderScript : MonoBehaviour
     private void Start()
     {
         // For now commented resolution fixing to see if other features can be expanded
-        // Screen.SetResolution(960,720,false);
+        Screen.SetResolution(960,720,false);
     }
 }
