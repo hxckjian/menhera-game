489118a619051321c69707cfe1ea387d77dde74f
Teh Hock Jian <hockjianteh@gmail.com>
2025-08-03 17:27:37 +0800

    Setup the baseline of the tutorialscene

---
+++ Assets/Scripts/InteractionScripts/YandereInteraction.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class YandereInteraction : MonoBehaviour, IInteractable
+{
+    [Header("Dialogue")]
+    [SerializeField] private GameObject dialogueCanvas;
+    [SerializeField] private Dialogue dialogue;
+
+    [Header("Required Direction for Trigger")]
+    [SerializeField] private Direction requiredFacingDirection = Direction.None;
+
+    public Direction RequiredDirection => requiredFacingDirection;
+
+    private void Start()
+    {
+        if (dialogueCanvas != null)
+        {
+            dialogueCanvas.SetActive(false);
+        }
+    }
+
+    public void Interact()
+    {
+        InteractionUI.Instance.ShowDialogueOnly(dialogueCanvas, dialogue);
+    }
+
+    private void OnSceneClick()
+    {
+        InteractionUI.Instance.Hide();
+    }
+}
