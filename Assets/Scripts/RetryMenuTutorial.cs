a73410ea1e3108ccd3d5baaac893ebee8d1d7d25
Teh Hock Jian <hockjianteh@gmail.com>
2025-08-05 19:35:36 +0800

    Fixed transition for tutorial scene

---
+++ Assets/Scripts/RetryMenuTutorial.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryMenuTutorial : MonoBehaviour
+{
+    //Called when retry button is clicked, loads the gameplay scene
+    public void OnRetryClick()
+    {
+        FindFirstObjectByType<SceneTransitionManager>().LoadSceneAndDestroy("TutorialScene");
+    }
+
+    // Called when exit button is clicked, return to start menu
+    public void OnExitClick()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
